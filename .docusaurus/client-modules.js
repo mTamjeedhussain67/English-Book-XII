0c8a1ae82b8cb5a18021eac853fd089b165f3432
Muhammad Tamjeed Hussain <166079389+mTamjeedhussain67@users.noreply.github.com>
2026-04-24 09:40:02 -0700

    Create client-modules.js

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("C:\\Users\\pc\\Downloads\\English Book XII\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("C:\\Users\\pc\\Downloads\\English Book XII\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\pc\\Downloads\\English Book XII\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\pc\\Downloads\\English Book XII\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\pc\\Downloads\\English Book XII\\src\\css\\custom.css"),
+];
